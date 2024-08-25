f0f865418ffeee7e6434732b5370b5dcbbe82af0
rita vinitsky <ritavinitsky@gmail.com>
2024-08-25 19:49:07 +0200

    v77

--- dist/src/models/user_model.js
+++ dist/src/models/user_model.js
@@ -1,5 +1,4 @@
 "use strict";
-// import mongoose, { Schema, Document } from 'mongoose';
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
@@ -24,83 +23,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// // Define the interface for input records
-// interface IInputRecord {
-//     food: string;
-//     cal: string;
-//     date: string; // Change from Date to string
-// }
-// export interface IUser extends Document {
-//     name: string;
-//     email: string;
-//     age: string;
-//     password: string;
-//     dailyCal: string;
-//     remaningCal: number;
-//     inputRecords: IInputRecord[];
-//     waterCups: string[];
-//     tokens: string[];
-// }
-// // Define the input record schema
-// const inputRecordSchema = new Schema<IInputRecord>({
-//     food: {
-//         type: String,
-//         required: true
-//     },
-//     cal: {
-//         type: String,
-//         required: true
-//     },
-//     date: {
-//         type: String, // Change from Date to String
-//         required: true
-//     }
-// });
-// // Define the user schema
-// const userSchema = new Schema<IUser>({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     age: {
-//         type: String,
-//         required: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     dailyCal: {
-//         type: String
-//     },
-//     remaningCal: {
-//         type: Number,
-//         default: 0
-//     },
-//     inputRecords: [inputRecordSchema],
-//      waterCups: {
-//         type: [String],
-//         default: Array(8).fill('blue')
-//     }
-//     tokens: {
-//         type: [String],
-//         default: []
-//     }
-// });
-// // Middleware to format date as YYYY-MM-DD before saving
-// inputRecordSchema.pre('save', function (next) {
-//     if (this.date) {
-//         const date = new Date(this.date);
-//         this.date = date.toISOString().split('T')[0]; // Format date as YYYY-MM-DD
-//     }
-//     next();
-// });
-// export default mongoose.model<IUser>('User', userSchema);
 const mongoose_1 = __importStar(require("mongoose"));
 // Define the input record schema
 const inputRecordSchema = new mongoose_1.Schema({
@@ -115,17 +37,6 @@ const inputRecordSchema = new mongoose_1.Schema({
     date: {
         type: String, // Change from Date to String
         required: true
-    }
-});
-// Define the water color schema
-const waterCupschema = new mongoose_1.Schema({
-    color: {
-        type: String,
-        default: 'blue'
-    },
-    date: {
-        type: String,
-        default: () => new Date().toISOString().split('T')[0] // Default to current date in YYYY-MM-DD format
     }
 });
 // Define the user schema
@@ -156,15 +67,15 @@ const userSchema = new mongoose_1.Schema({
     },
     inputRecords: [inputRecordSchema],
     waterCups: {
-        type: [waterCupschema],
-        default: Array(8).fill(null).map(() => ({ color: 'blue', date: new Date().toISOString().split('T')[0] })) // Default to 8 blue colors with the current date
+        type: [String],
+        default: Array(8).fill('blue') // or any default value you prefer
     },
     tokens: {
         type: [String],
         default: []
     }
 });
-// Middleware to format date as YYYY-MM-DD before saving input records
+// Middleware to format date as YYYY-MM-DD before saving
 inputRecordSchema.pre('save', function (next) {
     if (this.date) {
         const date = new Date(this.date);
--- src/models/user_model.ts
+++ src/models/user_model.ts
@@ -1,88 +1,3 @@
-// import mongoose, { Schema, Document } from 'mongoose';
-
-// // Define the interface for input records
-// interface IInputRecord {
-//     food: string;
-//     cal: string;
-//     date: string; // Change from Date to string
-// }
-
-// export interface IUser extends Document {
-//     name: string;
-//     email: string;
-//     age: string;
-//     password: string;
-//     dailyCal: string;
-//     remaningCal: number;
-//     inputRecords: IInputRecord[];
-//     waterCups: string[];
-//     tokens: string[];
-// }
-
-// // Define the input record schema
-// const inputRecordSchema = new Schema<IInputRecord>({
-//     food: {
-//         type: String,
-//         required: true
-//     },
-//     cal: {
-//         type: String,
-//         required: true
-//     },
-//     date: {
-//         type: String, // Change from Date to String
-//         required: true
-//     }
-// });
-
-// // Define the user schema
-// const userSchema = new Schema<IUser>({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     age: {
-//         type: String,
-//         required: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     dailyCal: {
-//         type: String
-//     },
-//     remaningCal: {
-//         type: Number,
-//         default: 0
-//     },
-//     inputRecords: [inputRecordSchema],
-//      waterCups: {
-//         type: [String],
-//         default: Array(8).fill('blue')
-//     }
-//     tokens: {
-//         type: [String],
-//         default: []
-//     }
-// });
-
-// // Middleware to format date as YYYY-MM-DD before saving
-// inputRecordSchema.pre('save', function (next) {
-//     if (this.date) {
-//         const date = new Date(this.date);
-//         this.date = date.toISOString().split('T')[0]; // Format date as YYYY-MM-DD
-//     }
-//     next();
-// });
-
-// export default mongoose.model<IUser>('User', userSchema);
-
 import mongoose, { Schema, Document } from 'mongoose';
 
 // Define the interface for input records
@@ -90,12 +5,6 @@ interface IInputRecord {
     food: string;
     cal: string;
     date: string; // Change from Date to string
-}
-
-// Define the interface for water colors
-interface IWaterColor {
-    color: string;
-    date: string; // Date in YYYY-MM-DD format
 }
 
 export interface IUser extends Document {
@@ -106,7 +15,7 @@ export interface IUser extends Document {
     dailyCal: string;
     remaningCal: number;
     inputRecords: IInputRecord[];
-    waterCups: IWaterColor[]; // Updated to use IWaterColor interface
+    waterCups: string[];
     tokens: string[];
 }
 
@@ -122,18 +31,6 @@ const inputRecordSchema = new Schema<IInputRecord>({
     },
     date: {
         type: String, // Change from Date to String
-        required: true
-    }
-});
-
-// Define the water color schema
-const waterCupschema = new Schema<IWaterColor>({
-    color: {
-        type: String,
-        default: 'blue'
-    },
-    date: {
-        type: String,
         required: true
     }
 });
@@ -165,23 +62,17 @@ const userSchema = new Schema<IUser>({
         default: 0
     },
     inputRecords: [inputRecordSchema],
-    waterCups:[waterCupschema],
+    waterCups: {
+        type: [String],
+        default: Array(8).fill('blue') // or any default value you prefer
+    },
     tokens: {
         type: [String],
         default: []
     }
 });
 
-// Middleware to format date as YYYY-MM-DD before saving input records
-waterCupschema.pre('save', function (next) {
-    if (this.date) {
-        const date = new Date(this.date);
-        this.date = date.toISOString().split('T')[0]; // Format date as YYYY-MM-DD
-    }
-    next();
-});
-
-// Middleware to format date as YYYY-MM-DD before saving input records
+// Middleware to format date as YYYY-MM-DD before saving
 inputRecordSchema.pre('save', function (next) {
     if (this.date) {
         const date = new Date(this.date);
